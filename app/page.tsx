6d54b425d6052c9ce58f659b0ddfb6693d01f770
k-utsumi <kaba.box@gmail.com>
2024-11-16 06:28:23 +0900

    ♻️ [home] レイアウトの組み替え

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
 			</div>
 
 			<div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-				<div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+				<div className="grid place-content-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
 					{/* <div className="relative w-0 h-0 border-[15px] border-t-0 border-b-[26px] border-transparent border-b-black" /> */}
 					<div className={styles.shape} />
 
@@ -28,12 +28,12 @@ export default function Page() {
 					</p>
 					<Link
 						href="/login"
-						className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+						className="justify-self-start flex items-center gap-5 rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
 					>
 						<span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
 					</Link>
 				</div>
-				<div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+				<div className="grid items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
 					<p>
 						<Link className="text-blue-500" href="/dashboard">
 							Dashboard
