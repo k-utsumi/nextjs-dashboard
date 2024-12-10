234308fd461ef058c47ece638b6d06631d29fbda
k-utsumi <kaba.box@gmail.com>
2024-12-10 17:59:08 +0900

    Best practice: Debouncing
    https://nextjs.org/learn/dashboard-app/adding-search-and-pagination#best-practice-debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,11 +2,21 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
 	const searchParams = useSearchParams();
 	const pathname = usePathname();
 	const { replace } = useRouter();
+
+	const handleSearch = useDebouncedCallback((term: string) => {
+		const params = new URLSearchParams(searchParams);
+
+		console.log(`Searching... ${term}`);
+
+		term ? params.set("query", term) : params.delete("query");
+		replace(`${pathname}?${params.toString()}`);
+	}, 300);
 
 	return (
 		<div className="relative flex flex-1 flex-shrink-0">
@@ -23,11 +33,4 @@ export default function Search({ placeholder }: { placeholder: string }) {
 			<MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
 		</div>
 	);
-
-	function handleSearch(term: string) {
-		const params = new URLSearchParams(searchParams);
-
-		term ? params.set("query", term) : params.delete("query");
-		replace(`${pathname}?${params.toString()}`);
-	}
 }
