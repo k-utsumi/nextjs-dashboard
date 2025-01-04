953e7ff85375d0b0881741f95a53357d5334242e
k-utsumi <kaba.box@gmail.com>
2025-01-04 14:56:37 +0900

    ♻️ スタイル調整

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -113,7 +113,7 @@ export const CreateForm = ({ customers }: Props) => {
 					</legend>
 					<div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
 						<div className="flex gap-4">
-							<div className="flex items-center">
+							<div className="flex gap-2 items-center">
 								<input
 									id="pending"
 									name="status"
@@ -125,12 +125,12 @@ export const CreateForm = ({ customers }: Props) => {
 								/>
 								<label
 									htmlFor="pending"
-									className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
+									className="flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
 								>
 									Pending <ClockIcon className="h-4 w-4" />
 								</label>
 							</div>
-							<div className="flex items-center">
+							<div className="flex gap-2 items-center">
 								<input
 									id="paid"
 									name="status"
@@ -142,7 +142,7 @@ export const CreateForm = ({ customers }: Props) => {
 								/>
 								<label
 									htmlFor="paid"
-									className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
+									className="flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
 								>
 									Paid <CheckIcon className="h-4 w-4" />
 								</label>
