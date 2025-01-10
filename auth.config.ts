41b0313e883acf88ce31d6120ed27d39ff4f63ca
k-utsumi <kaba.box@gmail.com>
2025-01-10 11:12:37 +0900

    Protecting your routes with Next.js Middleware (🚨 Error fixed)
    https://nextjs.org/learn/dashboard-app/adding-authentication#protecting-your-routes-with-nextjs-middleware

--- auth.config.ts
+++ auth.config.ts
@@ -4,4 +4,18 @@ export const authConfig = {
 	pages: {
 		signIn: "/login",
 	},
+	callbacks: {
+		authorized({ auth, request: { nextUrl } }) {
+			const isLoggedIn = !!auth?.user;
+			const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+			if (isOnDashboard) {
+				return isLoggedIn; // Redirect users to login if unauthenticated
+			}
+			if (isLoggedIn) {
+				return Response.redirect(new URL("/dashboard", nextUrl));
+			}
+			return true;
+		},
+	},
+	providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
