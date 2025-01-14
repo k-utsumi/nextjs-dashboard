8fd229aa51687ac452cce034dd6df0626c16fe89
k-utsumi <kaba.box@gmail.com>
2025-01-14 17:25:17 +0900

    Page title and descriptions 2
    https://nextjs.org/learn/dashboard-app/adding-metadata#page-title-and-descriptions

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -9,7 +9,7 @@ import type { Metadata } from "next";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-	title: "Invoices | Acme Dashboard",
+	title: "Invoices",
 };
 
 interface SearchParams {
